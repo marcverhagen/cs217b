feccd5af50f51e5608dd37bc7fc17b756a56ac9d
Marc Verhagen <marc@cs.brandeis.edu>
2026-04-14 15:19:29 -0400

    Some weeding

--- code/web-services/graphql/example2/api/mutations.py
+++ code/web-services/graphql/example2/api/mutations.py
@@ -1,19 +1,22 @@
 from datetime import date
 
-from ariadne import convert_kwargs_to_snake_case
 import sqlalchemy
 
 from api import db
 from api.models import Post
 
 
-@convert_kwargs_to_snake_case
+'''
+curl 127.0.0.1:5000/graphql -H 'content-type: application/json' -X POST -d '
+{
+  "query": "mutation CreateNewPost { createPost( title: \"New Blog Post\", description: \"Some Description\") { post { id title description created_at } success errors } }"
+}'
+'''
+
 def create_post_resolver(obj, info, title, description):
     try:
         today = date.today()
         post = Post(title=title, description=description, created_at=today)
-        print('>>>', type(obj), obj)
-        print('>>>', type(info), info)
         db.session.add(post)
         db.session.commit()
         return {
@@ -29,7 +32,6 @@ def create_post_resolver(obj, info, title, description):
             "errors": [f"{type(e)}: {e}"] }
 
 
-@convert_kwargs_to_snake_case
 def update_post_resolver(obj, info, id, title, description):
     try:
         post = Post.query.get(id)
@@ -47,7 +49,6 @@ def update_post_resolver(obj, info, id, title, description):
             "errors": ["item matching id {id} not found"] }
 
 
-@convert_kwargs_to_snake_case
 def delete_post_resolver(obj, info, id):
     try:
         post = Post.query.get(id)
